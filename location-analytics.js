82ebc12053c73117fe698ef5db70ea561b4ef4ec
rfahrne <rfahrner@wgu.edu>
2026-08-20 15:18:59 -0500

    zebra

--- location-analytics.js
+++ location-analytics.js
@@ -334,7 +334,7 @@ function renderTable() {
     if (row.rowType === 'day') {
       const zebra = dayIndex % 2 === 1;
       dayIndex += 1;
-      const cellStyle = zebra ? 'background:#6ca5d1;' : '';
+      const cellStyle = zebra ? 'background:#AED0E7;' : '';
       const dow = DAY_NAMES[new Date(row.date + 'T00:00:00').getDay()];
       const note = laState.notesByDate[row.date] || '';
       const metricCells = FIELD_DEFS.map((f) => rowTd(fmtValue(f, row[f.key]), cellStyle)).join('');
